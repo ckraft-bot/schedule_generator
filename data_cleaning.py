419539ca88e3ebac3de9168ebafff5a7690f5a69
Claire Kraft <79609609+ckraft-bot@users.noreply.github.com>
2024-03-15 17:41:25 -0400

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -17,5 +17,5 @@ latest_indices = raw_df.groupby('VOLUNTEER NAME')['TIMESTAMP'].idxmax()
 latest_rows = raw_df.loc[latest_indices]
 
 # Write the latest rows to a new CSV file
-clean_df = latest_rows.to_csv(r"C:\\Users\\Clair\\OneDrive\\Documents\\Volunteer\\clean_show_availability.csv", index=False)
+clean_df = latest_rows.to_csv(r"<path>\clean_show_availability.csv", index=False)
 print("Latest rows for each volunteer saved successfully.")
